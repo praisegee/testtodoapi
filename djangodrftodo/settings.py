bd12981ca23903b656060a3fc3be06b153e6fcae
Adesanmi Dayo PraiseGod <dayopraisegod@gmail.com>
2022-07-23 21:05:31 +0100

    heroku setup

--- djangodrftodo/settings.py
+++ djangodrftodo/settings.py
@@ -93,13 +93,13 @@ DATABASES = {
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'authentication.authentications.TokenAuthentication',
-        # 'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
     ],
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
     ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 1
+    'PAGE_SIZE': 10
 }
 
 # Password validation
