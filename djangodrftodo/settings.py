1aa360102f61c8e56259a735e62d702dd47359e3
PraiseGod <99336188+praisegee@users.noreply.github.com>
2022-09-12 02:30:41 +0100

    Update settings.py

--- djangodrftodo/settings.py
+++ djangodrftodo/settings.py
@@ -144,6 +144,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = [BASE_DIR / "static_files"]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
