2c79bcdacd633be22c7010f6d8c644a9caef1a25
Adesanmi Dayo PraiseGod <dayopraisegod@gmail.com>
2022-09-12 07:08:04 +0100

    settings

--- djangodrftodo/settings.py
+++ djangodrftodo/settings.py
@@ -159,7 +159,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
-# STATICFILES_DIRS = [BASE_DIR / "static_files"]
+STATICFILES_DIRS = [BASE_DIR / "staticfiles"]
 
 
 # Default primary key field type
