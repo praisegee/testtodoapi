aa4874dd80453f2de3b841a54f30df251494f54e
Adesanmi Dayo PraiseGod <dayopraisegod@gmail.com>
2022-09-12 07:04:13 +0100

    settings

--- djangodrftodo/settings.py
+++ djangodrftodo/settings.py
@@ -24,14 +24,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ["web-production-962a.up.railway.app"]
+ALLOWED_HOSTS = ["*"]
 
-try:
-    from local_settings import *
-except ImportError:
-    pass
+# try:
+#     from local_settings import *
+# except ImportError:
+#     pass
 
 # Application definition
 
